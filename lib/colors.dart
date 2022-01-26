6df20ffae37ba2afb02f0a69a8867297a59fc8d9
kellyketahr17 <kellykethar17@gmail.com>
2022-01-26 16:13:58 +0300

    Initial commit Material Components for Flutter

---
+++ lib/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color(0xFFFEEAE6);
+const kShrinePink100 = Color(0xFFFEDBD0);
+const kShrinePink300 = Color(0xFFFBB8AC);
+const kShrinePink400 = Color(0xFFEAA4A4);
+
+const kShrineBrown900 = Color(0xFF442B2D);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineBlue = Colors.blue;
+const kShrineBlack = Colors.black;
+const kShrineGrey = Colors.grey;
+const kShrineLightGrey = Colors.white24;
+const kShrineGreen = Colors.lightGreenAccent;
+
+
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
